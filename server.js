523f78bd244db4f03c9e041acce00b2f04ab3d29
baqer ali <124620943+noone313@users.noreply.github.com>
2025-06-27 04:04:14 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { startServer } from './models/models.js';
 import cookieParser from 'cookie-parser';
-import bodyParser from 'body-parser';
 import { config } from 'dotenv';
 import {userRouter} from './routes/user.route.js';
 import {fileRouter} from './routes/file.route.js';
@@ -12,8 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+
 
 
 app.use('/api/v1/users',userRouter);
